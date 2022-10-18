e90390acdece0bbccd6718c533ac0692b74887e2
Oris Walft <46623531+oriswalft@users.noreply.github.com>
2022-10-18 11:19:48 +0200

    J'ai galéré pour la suite 2 :'D

--- src/main/java/tp/trois/suite2.java
+++ src/main/java/tp/trois/suite2.java
@@ -1,7 +1,19 @@
 package tp.trois;
 
 public class suite2 {
-    public static void name() {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i != 20; i++) {
+            int c =  a + b;
+            System.out.print(" "+c);
+            a = b;
+            b = c;
+
+        }
+
+    
         
     }
 
