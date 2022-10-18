b2e1149bacc56a95ea65443e8f18f979da988afd
Oris Walft <46623531+oriswalft@users.noreply.github.com>
2022-10-18 11:27:05 +0200

    Ajout suite 3

---
+++ src/main/java/tp/trois/suite3.java
@@ -0,0 +1,11 @@
+package tp.trois;
+
+public class suite3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i * i + " ");
+
+        }
+    }
+    
+}
