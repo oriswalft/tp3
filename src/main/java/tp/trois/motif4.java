be2e0efd844e93f66aee936d82aebc14eea05d7e
Oris Walft <46623531+oriswalft@users.noreply.github.com>
2022-11-22 09:45:51 +0100

    Actualisation des fichiers

--- src/main/java/tp/trois/motif4.java
+++ src/main/java/tp/trois/motif4.java
@@ -1,10 +1,14 @@
 package tp.trois;
 
 public class motif4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
         int taille = 6;
-
-        for (int nombreDeLigne = taille; 
-
+        for (int nombreDeLigne = 1; nombreDeLigne <= taille; nombreDeLigne++) {
+        for (int nombreDeColonne = 1; nombreDeColonne <= nombreDeLigne;
+        nombreDeColonne++) {
+        System.out.print(nombreDeLigne);
+        }
+        System.out.println();
+        }
     }
 }
